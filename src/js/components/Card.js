e8b773e5e117b5266300e45b9991a674baa6dd4f
Casper Fabricius <me@casperfabricius.com>
2016-06-15 15:54:50 +0200

    More cards, also on the table.

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -3,23 +3,76 @@ import { Entity } from 'aframe-react'
 
 export default class Card extends Component {
   render () {
+    let geometry = { primitive: 'plane' }
+    Object.assign(geometry, this.getDimensions())
+
+    let material = {src: this.getSource()}
+    let position = this.getPosition()
+    let rotation = this.getRotation()
+
     return (
       <Entity
-        geometry={{primitive: 'plane', width: 0.10, height: 0.15}}
-        material={{src: this.imageSrc()}}
-        position="0 2 3"
+        geometry={geometry}
+        material={material}
+        position={position}
+        rotation={rotation}
       ></Entity>
     )
   }
 
-  imageSrc () {
+  getSource () {
     switch(this.props.type) {
       case 'response':
-        switch(this.props.color) {
-          case 'green':
-            return '#green-light'
-        }
-        break;
+        return `#${this.props.color}-light`
+      case 'area':
+        return `#${this.props.title}`
     }
   }
+
+
+  getDimensions () {
+    switch(this.props.type) {
+      case 'response':
+        return { width: 0.5, height: 0.75 }
+      case 'area':
+        return { width: 1, height: 1.5 }
+    }
+  }
+
+  getPosition () {
+    switch(this.props.type) {
+      case 'response':
+        if (this.props.selected) {
+          return '0 1.1 1.5'
+        } else {
+          let x;
+          switch(this.props.color) {
+            case 'green':
+              x = -0.75
+              break
+            case 'yellow':
+              x = 0
+              break
+            case 'red':
+              x = 0.75
+              break
+          }
+          return `${x} 1.5 0`
+        }
+      case 'area':
+        return "0 3 0"
+    }
+  }
+
+  getRotation () {
+    switch(this.props.type) {
+      case 'response':
+        if(this.props.selected) {
+          return '-90 0 0'
+        }
+    }
+
+    return '0 0 0'
+  }
+
 }
--- src/js/containers/Root.js
+++ src/js/containers/Root.js
@@ -10,13 +10,21 @@ export default class Root extends Component {
     return (
       <Scene altspace="usePixelScale: false; verticalAlign: bottom;" vr-mode-ui="enabled: false;">
         <a-assets>
-          <img id="green-light" src="images/green_light.png" />
-          <img id="yellow-light" src="images/yellow_light.png" />
-          <img id="red-light" src="images/red_light.png" />
+          <img id="green-light" src="images/response_cards/green_light.png" />
+          <img id="yellow-light" src="images/response_cards/yellow_light.png" />
+          <img id="red-light" src="images/response_cards/red_light.png" />
+          <img id="delivering-value" src="images/area_cards/delivering_value.png" />
         </a-assets>
 
         <Table />
-        <Card type="response" color="green" />
+
+        <Card type="area" title="delivering-value" />
+
+        <Card type="response" color="green" selected={false} />
+        <Card type="response" color="yellow" selected={false} />
+        <Card type="response" color="red" selected={false} />
+
+        <Card type="response" color="green" selected={true} />
       </Scene>
     )
   }
