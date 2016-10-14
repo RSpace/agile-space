4ebe6c8875db8c8c18173a9b6984b99f98e8284a
Casper Fabricius <me@casperfabricius.com>
2016-10-14 14:50:53 +0200

    Bigger response card tips that sits underneath instead of above.

--- src/js/components/ResponseTip.js
+++ src/js/components/ResponseTip.js
@@ -3,9 +3,9 @@ import { Entity } from 'aframe-react'
 
 export default class ResponseTip extends Component {
   render () {
-    let geometry = { primitive: 'plane', width: 0.34, height: 0.2108 }
+    let geometry = { primitive: 'plane', width: 0.34, height: 0.34 }
     let material = {src: `#${this.props.color}-tip`, side: 'double', transparent: 'true'}
-    let position = "0 0.1875 0.11"
+    let position = "0 -0.25 -0.15"
     let rotation = "90 0 0"
 
     if (this.props.visible) {
