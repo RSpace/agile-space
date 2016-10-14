6382d1bfae1f5072cb6c89cf798f06c6ffb03732
Casper Fabricius <me@casperfabricius.com>
2016-10-14 22:25:29 +0200

    Changed how sounds are loaded and played once again, to try and get sounds working well in Altspace.

--- src/js/entrypoint.js
+++ src/js/entrypoint.js
@@ -6,10 +6,11 @@ import { createStore } from 'redux'
 import configureStore from './store/store'
 import { INITIAL_STATE, getPlayerInfo } from './core'
 import { initRead, saveUser } from './store/sync'
+import { loadSounds } from './helpers/SoundManager'
 
 const store = configureStore(INITIAL_STATE)
 
-Promise.all([initRead(store), getPlayerInfo()]).then((values) => {
+Promise.all([initRead(store), getPlayerInfo(), loadSounds()]).then((values) => {
   let store, playerInfo
   [store, playerInfo] = values
   saveUser(playerInfo.id, playerInfo.name)
--- src/js/helpers/SoundManager.js
+++ src/js/helpers/SoundManager.js
@@ -1,23 +1,63 @@
 import { isOverview } from '../core'
 
+var AudioContext = window.AudioContext || window.webkitAudioContext
+var audioContext = new AudioContext()
+
+function AppAudio (url) {
+  this.volume = 1
+  this.loaded = new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest()
+    req.open('GET', url)
+    req.responseType = 'arraybuffer'
+    req.onload = function () {
+      audioContext.decodeAudioData(req.response, function (buffer) {
+        this.buffer = buffer
+        resolve()
+      }.bind(this))
+    }.bind(this)
+    req.send()
+  }.bind(this))
+}
+
+AppAudio.prototype.play = function () {
+	var source = audioContext.createBufferSource()
+	source.buffer = this.buffer
+
+	var gainNode = audioContext.createGain()
+	gainNode.gain.value = this.volume
+	source.connect(gainNode)
+	gainNode.connect(audioContext.destination)
+
+	source.start(0)
+}
+
+let responseSound
+let areaChangeSound
+export function loadSounds() {
+  responseSound = new AppAudio("sounds/cardup.ogg")
+  areaChangeSound = new AppAudio("sounds/crystalglass.ogg")
+
+  responseSound.volume = 0.5
+  areaChangeSound.volume = 0.5
+
+  return Promise.all([
+    responseSound.loaded,
+    areaChangeSound.loaded
+  ])
+}
+
 export function playResponseSound() {
-  play("response-sound")
+  play(responseSound)
 }
 
 export function playAreaChangeSound() {
-  play("area-change-sound")
+  play(areaChangeSound)
 }
 
-function play(elementId) {
+function play(sound) {
   if (isOverview()) {
     return
   }
 
-  let el = document.getElementById(elementId)
-  if (el) {
-    el.play()
-    console.log("Playing " + elementId)
-  } else {
-    console.log("Unable to find element " + elementId + " and play sound.")
-  }
+  sound.play()
 }
